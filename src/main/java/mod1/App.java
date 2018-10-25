90283a8e3cb2670b690612f0ef37040484f3710d
jraleman <jraleman@bendiburg.com>
2018-10-25 04:11:09 -0700

    Fix package declaration error

--- src/main/java/mod1/App.java
+++ src/main/java/mod1/App.java
@@ -1,10 +1,10 @@
-package 42.jraleman.mod1;
+package mod1;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
--- src/test/java/mod1/AppTest.java
+++ src/test/java/mod1/AppTest.java
@@ -1,4 +1,4 @@
-package 42.jraleman.mod1;
+package mod1;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
     extends TestCase
 {
     /**
