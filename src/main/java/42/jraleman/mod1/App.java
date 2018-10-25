6e5d9e455ae972e2f0714d3ecf5f192c42b6d4f2
jraleman <jraleman@bendiburg.com>
2018-10-25 04:07:07 -0700

    Update project structure using mvn archetype:generate

---
+++ src/main/java/42/jraleman/mod1/App.java
@@ -0,0 +1,13 @@
+package 42.jraleman.mod1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
